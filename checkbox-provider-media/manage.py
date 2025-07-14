61931ab5d186baf95a3cf00232903336ca84b4d8
Shane McKee <shane.mckee@canonical.com>
2025-07-14 15:42:15 -0600

    Fix formatting issue reported by Black

--- checkbox-provider-media/manage.py
+++ checkbox-provider-media/manage.py
@@ -14,7 +14,7 @@ from plainbox.provider_manager import setup, N_
 # (optional)
 
 setup(
-    name='com.canonical.certification:media',
+    name="com.canonical.certification:media",
     version="1.0",
     description=N_("The com.canonical.certification:media provider"),
     gettext_domain="com_canonical_certification_media",
